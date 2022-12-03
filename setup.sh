aed4c0b35522f9f76fba9e496f1597592cc7fae1
Anil Ozyalcin <aeozyalcin@gmail.com>
2022-12-03 11:36:09 -0800

    modified:   requirements.txt 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get install python3 python3-pip libglib2.0-dev
-pip3 install -r requirements.txt
+sudo apt install python3 python3-pip libglib2.0-dev bluez bluetooth libbluetooth-dev
+sudo pip3 install -r requirements.txt
 #HELPER_LOC="$(pip3 show bluepy | grep Location: | cut -c 11-)/bluepy/bluepy-helper"
 #CAPABILITIES='cap_net_raw,cap_net_admin+eip'
 #sudo setcap $CAPABILITIES "$HELPER_LOC"
